53ab570285ba15f708fce2a596462eec1fecc207
gillkyle <kylerobertgill@gmail.com>
2017-12-06 15:03:00 -0700

    first style updates

--- NL/App_Start/BundleConfig.cs
+++ NL/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace NL
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/custom.css"));
         }
     }
 }
